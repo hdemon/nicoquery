3a30e7567b82c389d4d57d8b2380d979688ce6f5
Masami Yonehara <zeitdiebe@gmail.com>
2013-08-08 21:35:40 +0900

    fix a mistake

--- lib/nicoquery.rb
+++ lib/nicoquery.rb
@@ -3,8 +3,9 @@ require "nicoquery/version"
 
 module NicoQuery
   def tag_search(tag: tag, sort: sort, order: order, &block)
-    NicoCrawler::TagSearch.execute(tag: tag, sort: sort, order: order, &block)
+    NicoQuery::Crawler::TagSearch.execute(tag: tag, sort: sort, order: order, &block)
   end
 
   module_function :tag_search
 end
+;
--- lib/nicoquery/crawler/version.rb
+++
@@ -1,3 +0,0 @@
-module Nicocrawler
-  VERSION = "0.0.2.1"
-end
