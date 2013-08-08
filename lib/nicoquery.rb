14d38cc3b8fe40b717b47165dee65917771d33e3
Masami Yonehara <zeitdiebe@gmail.com>
2013-08-08 21:28:26 +0900

    fix a misstake at requirement

--- lib/nicoquery.rb
+++ lib/nicoquery.rb
@@ -1,6 +1,4 @@
-require "nicoquery/source/version"
-
-require "nicoquery/source"
+require "nicoquery/version"
 
 
 module NicoQuery
@@ -8,31 +6,5 @@ module NicoQuery
     NicoCrawler::TagSearch.execute(tag: tag, sort: sort, order: order, &block)
   end
 
-  module_function :tag_search
-end
-
-
-
-module NicoQuery
-  def getthumbinfo(video_id)
-    instance = NicoQuery::GetThumbInfo.new
-    instance.set video_id
-    instance.get
-  end
-
-  def video_array(video_id_array)
-    instance = NicoQuery::VideoArray.new
-    instance.set video_id_array
-    instance.get
-  end
-
-  def tag_search(tag: tag, sort: sort, order: order, page: page)
-    instance = NicoQuery::TagSearch.new
-    instance.set tag: tag, sort: sort, order: order, page: page
-    instance.get
-  end
-
-  module_function :getthumbinfo
-  module_function :video_array
   module_function :tag_search
 end
