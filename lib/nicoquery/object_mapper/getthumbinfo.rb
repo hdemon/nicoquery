8d248b8a88c802af3a90e8b1e7e74319eb65a6ca
Masami Yonehara <zeitdiebe@gmail.com>
2013-08-19 23:19:33 +0900

    refactor

--- lib/nicoquery/object_mapper/getthumbinfo.rb
+++ lib/nicoquery/object_mapper/getthumbinfo.rb
@@ -1,6 +1,5 @@
 require "nicoquery/object_mapper/base"
 require "nori"
-require "pry"
 
 
 module NicoQuery
@@ -125,37 +124,37 @@ module NicoQuery
   end
 end
 
-  # <nicovideo_thumb_response status="ok">
-  #   <thumb>
-  #     <video_id>sm9</video_id>
-  #     <title>新・豪血寺一族 -煩悩解放 - レッツゴー！陰陽師</title>
-  #     <description>レッツゴー！陰陽師（フルコーラスバージョン）</description>
-  #     <thumbnail_url>http://tn-skr2.smilevideo.jp/smile?i=9</thumbnail_url>
-  #     <first_retrieve>2007-03-06T00:33:00+09:00</first_retrieve>
-  #     <length>5:19</length>
-  #     <movie_type>flv</movie_type>
-  #     <size_high>21138631</size_high>
-  #     <size_low>17436492</size_low>
-  #     <view_counter>13536149</view_counter>
-  #     <comment_num>4148196</comment_num>
-  #     <mylist_counter>133688</mylist_counter>
-  #     <last_res_body>へん? はははははははははは wwwwwwwwwwwwwwwwwwww ...</last_res_body>
-  #     <watch_url>http://www.nicovideo.jp/watch/sm9</watch_url>
-  #     <thumb_type>video</thumb_type>
-  #     <embeddable>1</embeddable>
-  #     <no_live_play>0</no_live_play>
-  #     <tags domain="jp">
-  #       <tag lock="1">陰陽師</tag>
-  #       <tag lock="1">レッツゴー！陰陽師</tag>
-  #       <tag lock="1">公式</tag>
-  #       <tag lock="1">音楽</tag>
-  #       <tag lock="1">ゲーム</tag>
-  #       <tag>β時代の英雄</tag>
-  #       <tag>運営のお気に入り</tag>
-  #       <tag>最古の動画</tag>
-  #       <tag>1000万再生</tag>
-  #       <tag>弾幕動画</tag>
-  #     </tags>
-  #     <user_id>4</user_id>
-  #   </thumb>
-  # </nicovideo_thumb_response>
+# <nicovideo_thumb_response status="ok">
+#   <thumb>
+#     <video_id>sm9</video_id>
+#     <title>新・豪血寺一族 -煩悩解放 - レッツゴー！陰陽師</title>
+#     <description>レッツゴー！陰陽師（フルコーラスバージョン）</description>
+#     <thumbnail_url>http://tn-skr2.smilevideo.jp/smile?i=9</thumbnail_url>
+#     <first_retrieve>2007-03-06T00:33:00+09:00</first_retrieve>
+#     <length>5:19</length>
+#     <movie_type>flv</movie_type>
+#     <size_high>21138631</size_high>
+#     <size_low>17436492</size_low>
+#     <view_counter>13536149</view_counter>
+#     <comment_num>4148196</comment_num>
+#     <mylist_counter>133688</mylist_counter>
+#     <last_res_body>へん? はははははははははは wwwwwwwwwwwwwwwwwwww ...</last_res_body>
+#     <watch_url>http://www.nicovideo.jp/watch/sm9</watch_url>
+#     <thumb_type>video</thumb_type>
+#     <embeddable>1</embeddable>
+#     <no_live_play>0</no_live_play>
+#     <tags domain="jp">
+#       <tag lock="1">陰陽師</tag>
+#       <tag lock="1">レッツゴー！陰陽師</tag>
+#       <tag lock="1">公式</tag>
+#       <tag lock="1">音楽</tag>
+#       <tag lock="1">ゲーム</tag>
+#       <tag>β時代の英雄</tag>
+#       <tag>運営のお気に入り</tag>
+#       <tag>最古の動画</tag>
+#       <tag>1000万再生</tag>
+#       <tag>弾幕動画</tag>
+#     </tags>
+#     <user_id>4</user_id>
+#   </thumb>
+# </nicovideo_thumb_response>
